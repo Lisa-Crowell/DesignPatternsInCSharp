5b1ed04936cff2f23115c6c8f07ec4f83ac7972a
lisa-crowell <lisa.l.crowell@gmail.com>
2023-01-03 09:25:33 -0600

    Monostate example of the Singleton Pattern

---
+++ SingletonPattern/Monostate.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using static System.Console;
+
+namespace SingletonPattern;
+
+public class Monostate // Monostate example of the Singleton Pattern
+{
+    public class ChiefExecutiveOfficer
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new ChiefExecutiveOfficer();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new ChiefExecutiveOfficer();
+            WriteLine(ceo2);
+        }
+    }
+}
